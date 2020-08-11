166fa5e54f1b4dc340f7a12c054d67d776348dda
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 01:50:34 +0200

    Create TIMER0_Register.h

---
+++ AVR32_Interfacing_Architecture/TIMER0_DRIVER/TIMER0_Register.h
@@ -0,0 +1,27 @@
+/****************************************************************/
+/*   Author             :    AbdElrahman Ibrahim Zaki		    */
+/*	 Date 				:    1st of July 2020 					*/
+/*	 Version 			:    1.0V 							 	*/
+/*	 Description 		:    TIMER0 Registers Addresses              */
+/****************************************************************/
+
+#ifndef TIMER0_REG_H
+#define TIMER0_REG_H
+
+/* Timer/Counter Control Register */
+#define  TCCR0     *((volatile u8 *) 0X53)
+
+/* Output Compare Register */
+#define  OCR0     *((volatile u8 *) 0X5C)
+
+/* Timer/Counter Register */
+#define  TCNT0     *((volatile u8 *) 0X52)
+
+/* Timer/Counter Interrupt Mask Register */
+#define  TIMSK     *((volatile u8 *) 0X59)
+
+/* Timer/Counter Interrupt Flag Register */
+#define  TIFR     *((volatile u8 *) 0X58)
+
+#endif
+
