f2b04cc19ac79ee2518ab831fa2faba39af0cd1a
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 01:57:35 +0200

    Create TIMER2_Register.h

---
+++ AVR32_Interfacing_Architecture/TIMER2_DRIVER/TIMER2_Register.h
@@ -0,0 +1,32 @@
+/****************************************************************/
+/*   Author             :    AbdElrahman Ibrahim Zaki		    */
+/*	 Date 				:    10th of July 2020 					*/
+/*	 Version 			:    1.0V 							 	*/
+/*	 Description 		:    TIMER2 Registers Addresses                     */
+/****************************************************************/
+
+#ifndef TIMER2_REG_H
+#define TIMER2_REG_H
+
+/* Timer2 Control Register */
+#define  TCCR2     *((volatile u8 *) 0X45)
+
+/* Output Compare Register */
+#define  OCR2     *((volatile u8 *) 0X43)
+
+/* Timer2 Register */
+#define  TCNT2     *((volatile u8 *) 0X44)
+
+/* Timer2 Interrupt Mask Register */
+#define  TIMSK     *((volatile u8 *) 0X59)
+
+/* Timer2 Interrupt Flag Register */
+#define  TIFR     *((volatile u8 *) 0X58)
+
+/* Timer2 Asynchronous Status Register
+to be connected either with RTC "Real Time Clock" or 
+to work as a timer  */
+#define  ASSR     *((volatile u8 *) 0X42)
+
+#endif
+
