d44dc80064dd615cf4e3b3334f18d0cdc029df81
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 01:34:25 +0200

    Create DIO_Registers.h

---
+++ AVR32_Interfacing_Architecture/DIO_DRIVER/DIO_Registers.h
@@ -0,0 +1,44 @@
+/*************************************************************/
+/*  AUTHOR     : AbdElrahman Ibrahim Zaki                    */
+/*  DATE       : 10th of June 2020										 */
+/*  VERSION	   : V0.1										             */
+/*  DESCRIPTION: DIO Registers Addresses                                  */
+/*                                                           */
+/*************************************************************/
+
+#ifndef DIO_REG_H
+#define DIO_REG_H
+
+// insert registers names and addresses
+
+		// PORTA Registers
+#define DDRA_REG  *((volatile u8 *) 0X3A)
+
+#define PORTA_REG *((volatile u8 *) 0X3B)
+
+#define PINA_REG  *((volatile u8 *) 0X39)
+
+		// PORTB Registers
+#define DDRB_REG  *((volatile u8 *) 0X37)
+
+#define PORTB_REG *((volatile u8 *) 0X38)
+
+#define PINB_REG  *((volatile u8 *) 0X36)
+
+		// PORTC Registers
+#define DDRC_REG  *((volatile u8 *) 0X34)
+
+#define PORTC_REG *((volatile u8 *) 0X35)
+
+#define PINC_REG  *((volatile u8 *) 0X33)
+
+		// PORTD Registers
+#define DDRD_REG  *((volatile u8 *) 0X31)
+
+#define PORTD_REG *((volatile u8 *) 0X32)
+
+#define PIND_REG  *((volatile u8 *) 0X30)
+
+
+
+#endif
