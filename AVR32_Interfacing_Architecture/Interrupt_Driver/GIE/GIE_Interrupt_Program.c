9dda648573e050d5f1cfc3beda2e5f3a8729b2bd
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 01:43:01 +0200

    Create GIE_Interrupt_Program.c

---
+++ AVR32_Interfacing_Architecture/Interrupt_Driver/GIE/GIE_Interrupt_Program.c
@@ -0,0 +1,22 @@
+/****************************************************************/
+/*   Author          :    AbdElrahman Ibrahim Zaki		    */
+/*	 Date 				:    25th of June 2020 				      	*/
+/*	 Version 			:    1.0V 							 	              */
+/*	 Description 		:    General Interrupt Program Configuration    */
+/****************************************************************/
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "GIE_int.h"
+#include "GIE_reg.h"
+
+// enable and disable global interrupt through these functions
+void GIE_voidEnable(void)
+{
+	SET_BIT(SREG , 7);
+}
+
+void GIE_voidDisable(void)
+{
+	CLR_BIT(SREG , 7);
+}
