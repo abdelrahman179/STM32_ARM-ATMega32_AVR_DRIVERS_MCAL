60abe263e1d1ce643b4ea36e41cf77f2f12554bc
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 01:27:02 +0200

    Create ADC_registers.h

---
+++ AVR32_Interfacing_Architecture/ADC_DRIVER/ADC_registers.h
@@ -0,0 +1,30 @@
+/****************************************************************/
+/*   Author       :    AbdElrahman Ibrahim Zaki         		    */
+/*	 Date 				:    23rd of June 2020 			              		*/
+/*	 Version 			:    1.0V 							 	                    */
+/*	 Description 	:    Registers addresses                      */
+/****************************************************************/
+
+#ifndef  ADC_REG_H
+#define  ADC_REG_H
+
+
+// Multiplexing Register
+#define   ADMUX   *((volatile u8 *) 0X27 ) 
+
+// Control and Status Register
+#define   ADCSRA  *((volatile u8 *) 0X26 ) 
+
+// Analog Digital High Register
+#define    ADCH   *((volatile u8 *) 0X25 ) 
+
+// Analog Digital LOW Register
+#define    ADCL   *((volatile u8 *) 0X24 )
+
+// Special Function I/O Register
+#define    SFIOR  *((volatile u8 *) 0X50 )
+
+// Read ADCL + ADCH both together u8 + u8
+#define    ADC_Register   *((volatile u16 *) 0X24 )
+
+#endif
