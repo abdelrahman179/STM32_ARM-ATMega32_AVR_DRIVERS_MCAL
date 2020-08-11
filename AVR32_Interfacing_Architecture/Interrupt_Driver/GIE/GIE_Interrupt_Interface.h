1c1c22d75cab3426c92fbf5ee2815c08f68f35ef
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 01:45:11 +0200

    Create GIE_Interrupt_Interface.h

---
+++ AVR32_Interfacing_Architecture/Interrupt_Driver/GIE/GIE_Interrupt_Interface.h
@@ -0,0 +1,14 @@
+/****************************************************************/
+/*   Author             :    AbdElrahman Ibrahim Zaki		    */
+/*	 Date 				:    25th of June 2020 					*/
+/*	 Version 			:    1.0V 							 	*/
+/*	 Description 		:    General Interrupt Interface file      */
+/****************************************************************/
+
+#ifndef  GIE_INT_H
+#define  GIE_INT_H
+
+void GIE_voidEnable(void);
+void GIE_voidDisable(void);
+
+#endif
