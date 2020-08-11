7411d000a5a22144317823349a8a32a1cb751bac
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 01:46:23 +0200

    Create Interrupt_Config.h

---
+++ AVR32_Interfacing_Architecture/Interrupt_Driver/Interrupt_Config.h
@@ -0,0 +1,19 @@
+/****************************************************************/
+/*   Author             :    AbdElrahman Ibrahim Zaki		    */
+/*	 Date 				:    26th of June 2020 					*/
+/*	 Version 			:    1.0V 							 	*/
+/*	 Description 		:    Interrupt User Manual                        */
+/****************************************************************/
+
+#ifndef  INT_CONFIG_H
+#define  INT_CONFIG_H
+
+
+#define   INTERRUPT_CH     EXT0
+
+ /* Pick up the Sense mode */
+#define   SENSE_MODE       FALLING_EDGE
+
+
+
+#endif
