bd974ac4542d0afbfaf820473d66beb3e225493b
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-10-15 12:39:19 +0200

    Update and rename TIMER1_Program.c to TIMER1_program.c

--- AVR32_Interfacing_Architecture/TIMER1_DRIVER/TIMER1_program.c
+++ AVR32_Interfacing_Architecture/TIMER1_DRIVER/TIMER1_program.c
@@ -2,13 +2,14 @@
 /*   Author             :    AbdElrahman Ibrahim Zaki		    */
 /*	 Date 				:    5th of July 2020 					*/
 /*	 Version 			:    1.0V 							 	*/
-/*	 Description 		:    TIMER1 Program                         */
+/*	 Description 		:    TIMER1 Program                     */
 /****************************************************************/
 
 #include "STD_TYPES.h"
 #include "BIT_MATH.h"
-#include "TIMER1_int.h"
-#include "TIMER1_reg.h"
+
+#include "TIMER1_interface.h"
+#include "TIMER1_private.h"
 #include "TIMER1_config.h"
 
 void (*TIMER1_OV_CallBack) (void);
