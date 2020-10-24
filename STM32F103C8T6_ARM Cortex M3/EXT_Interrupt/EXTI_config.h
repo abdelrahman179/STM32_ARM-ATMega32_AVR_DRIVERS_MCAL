8601f9f8ebdec5cae143978b91255f9e10bac644
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-10-25 00:58:09 +0200

    Create EXTI_config.h

---
+++ STM32F103C8T6_ARM Cortex M3/EXT_Interrupt/EXTI_config.h
@@ -0,0 +1,24 @@
+/*******************************************************************************/
+/* Author : AbdElrahman I.Zaki                                                 */
+/* Date   : 21 August 2020                                                     */
+/* Version: V01                                                                */
+/* Desc.  : STM32F103C8T6 ARM Cortex M3 EXTI User Manual                       */
+/*******************************************************************************/
+
+
+#ifndef EXTI_CONFIG_H
+#define EXTI_CONFIG_H
+
+/*  Range : LINE0 - LINE15   */
+#define EXTI_LINE    LINE0   
+
+/*  Range : - RISING_EDGE
+            - FALLING_EDGE
+            - ON_CHANGE   */
+#define EXTI_MODE    FALLING_EDGE      
+
+
+
+
+
+#endif
