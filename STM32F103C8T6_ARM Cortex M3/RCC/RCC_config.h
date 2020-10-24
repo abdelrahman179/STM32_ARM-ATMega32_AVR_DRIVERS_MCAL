0b18faf2c3c59bf6ef0c5f7c85acfdff102cb421
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-10-25 00:52:27 +0200

    Update RCC_config.h

--- STM32F103C8T6_ARM Cortex M3/RCC/RCC_config.h
+++ STM32F103C8T6_ARM Cortex M3/RCC/RCC_config.h
@@ -1,44 +1,103 @@
 /**********************************************************************************/
 /* Author : AbdElrahman I.Zaki                                                    */
 /* Date   : 8 August 2020                                                         */
-/* Version: V01                                                                   */
+/* Version: V02                                                                   */
 /* Desc.  : STM32F103C8T6 ARM Cortex M3 Reset & Clock Control "RCC" User Manual   */
 /**********************************************************************************/
 
 #ifndef RCC_CONFIG_H
 #define RCC_CONFIG_H
 
-/* Options: RCC_HSE_CRYSTAL
-            RCC_HSE_RC
-            RCC_HSI
-            RCC_PLL      */
-#define RCC_CLOCK_TYPE   RCC_HSE_CRYSTAL
 
-/* Note: Select a value only if you have PLL as an input clock source */
-/* Options: RCC_PLL_IN_HSI_DIV_2
-            RCC_PLL_IN_HSE_DIV_2
-            RCC_PLL_IN_HSE       */
-#if (RCC_CLOCK_TYPE == RCC_PLL)
-    #define RCC_PLL_INPUT   RCC_PLL_IN_HSE_DIV_2
-#endif
+/* Options : 
+			RCC_HSI 		  
+			RCC_HSE_CRYSTAL   
+			RCC_HSE_RC       
+			RCC_PLL 			 
+*/
+#define SYS_CLOCK         RCC_HSE_CRYSTAL
 
-/* Note: Select a value only if you have PLL as an input clock source */
-/* Caution: The PLL output frequency must not exceed 72 MHz           */
-/* These bits has to be set only when PLL is disabled                 */
-/* Options: 2 -> 16 */ 
-#if (RCC_CLOCK_TYPE == RCC_PLL)
-    #define RCC_PLL_MUL_VAL    4    
-#endif
+/* 
+    Clock Security System : (ENABLE - DISABLE)
+    Security Sys that indicates HSE errors, fire interrupt and switch to HSI CLK 
+*/
+#define CSS_ON            DISABLE
 
-/* Microcontroller output clock, used to measure clock error          */
-/* It's used for: - Diagnostics 
+/*
+   Options : -16 - 15 
+*/ 
+#define HSI_STEP_CORRECTION   0 
+
+/*
+  Options :
+			RCC_PLL_HSI_BY2   
+			RCC_PLL_HSE       
+			RCC_PLL_HSE_BY2
+*/ 
+#define RCC_PLL_SOURCE    HSI_CLOCK
+
+/*
+  Options : 2 - 16 
+*/ 
+#define RCC_PLL_MUL       2 
+
+/* Microcontroller output clock, used to measure clock error          
+    It's used for: - Diagnostics 
                   - C/K Cascading 
-   Range : - No Clock
-           - System Clock
-           - HSI
-           - HSE
-           - PLL divided by 2        */
-#define RCC_MCO_SELECTION   NO_CLOCK
+  Options : 
+			NO_CLOCK      
+			HSI_CLOCK     
+			HSE_CLOCK     
+			PLL_CLOCK     
+			SYSTEM_CLOCK
+*/ 
+#define RCC_MCO_PIN       NO_CLOCK
+
+/*
+  Options :  		  
+			RCC_PRESCALER_2   
+			RCC_PRESCALER_4 
+			RCC_PRESCALER_6
+			RCC_PRESCALER_8
+*/ 
+#define RCC_ADC_PRESCALER    RCC_PRESCALER_2
+
+/*
+  Options : 
+			RCC_PRESCALER_0 		  
+			RCC_PRESCALER_2   
+			RCC_PRESCALER_4 
+			RCC_PRESCALER_8 
+			RCC_PRESCALER_16
+*/ 
+#define RCC_APB2_PRESCALER   RCC_PRESCALER_0
+
+/*
+  Options : 
+			RCC_PRESCALER_0 		  
+			RCC_PRESCALER_2   
+			RCC_PRESCALER_4 
+			RCC_PRESCALER_8 
+			RCC_PRESCALER_16
+*/
+/* Note : this bus clock must not exceed 36 MHZ*/
+#define RCC_APB1_PRESCALER   RCC_PRESCALER_0
+
+/*
+  Options : 
+			RCC_PRESCALER_0 		  
+			RCC_PRESCALER_2
+			RCC_PRESCALER_4			
+			RCC_PRESCALER_8 
+			RCC_PRESCALER_16
+			RCC_PRESCALER_64
+			RCC_PRESCALER_128
+			RCC_PRESCALER_256
+			RCC_PRESCALER_512			
+*/
+#define RCC_AHB_PRESCALER    RCC_PRESCALER_0
+
+
 
 
 
