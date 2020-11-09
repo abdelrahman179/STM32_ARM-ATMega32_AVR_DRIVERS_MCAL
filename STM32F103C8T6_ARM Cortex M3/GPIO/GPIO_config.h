8444a560a6f0b74c633c254998b95522ae55a937
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-09 22:17:39 +0200

    Update GPIO_config.h

--- STM32F103C8T6_ARM Cortex M3/GPIO/GPIO_config.h
+++ STM32F103C8T6_ARM Cortex M3/GPIO/GPIO_config.h
@@ -1,12 +1,12 @@
 /**********************************************************/
 /* Author : AbdElrahman I.Zaki                            */
 /* Date   : 10 August 2020                                */
-/* Version: V01                                           */
+/* Version: V02                                           */
 /* Desc.  : STM32F103C8T6 ARM Cortex M3 GPIO user manual  */
 /**********************************************************/
 
-#ifndef GPIO_CONFIG_H
-#define GPIO_CONFIG_H
+#ifndef _GPIO_CONFIG_H_
+#define _GPIO_CONFIG_H_
 
 
 
