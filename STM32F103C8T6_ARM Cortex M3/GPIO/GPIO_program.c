aa748466406df80de18cb6f99085065fe717bfb9
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-09 22:16:52 +0200

    Update GPIO_program.c

--- STM32F103C8T6_ARM Cortex M3/GPIO/GPIO_program.c
+++ STM32F103C8T6_ARM Cortex M3/GPIO/GPIO_program.c
@@ -1,7 +1,7 @@
 /*********************************************************/
 /* Author : AbdElrahman I.Zaki                           */
 /* Date   : 10 August 2020                               */
-/* Version: V01                                          */
+/* Version: V02                                          */
 /* Desc.  : STM32F103C8T6 ARM Cortex M3 GPIO program     */
 /*********************************************************/
 
@@ -14,7 +14,7 @@
 
 
             /* ------ Function that sets the pin "mode and configuration" ------- */
-void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , GPIO_Modes Copy_MODE)
+void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , u8 Copy_u8MODE)
 {
     switch (Copy_PORT)
     {                      
@@ -25,9 +25,9 @@ void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , G
                             /*  Reset the chosen PIN which consists of 4 bits not all the CRL Register 
                                 As long as we will use logical operation we have to reset first "not to over-write"  */
                                 // Note : We can use 4 SET-PINss but it consumes more clocks                              
-                            GPIOA_CRL &= ~ ((0b1111) << (Copy_PIN * 4));
+                            MGPIOA -> CRL &= ~ ((0b1111) << (Copy_PIN * 4));
                             /* Set the chosen MODE on the chosen PIN */
-                            GPIOA_CRL |=   ((Copy_MODE) << (Copy_PIN * 4));
+                            MGPIOA -> CRL |=   ((Copy_u8MODE) << (Copy_PIN * 4));
                         }
                     /* Check if Pin lies between 8-15 noting that each pin consists of 4 bits "CRH Configuration Register High" */
                     else if (Copy_PIN <= 15)
@@ -35,9 +35,9 @@ void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , G
                             /* To get the exact chosen Pin when it's multiplied by 4 */
                             Copy_PIN = Copy_PIN - 8;
                             /* Reset the chosen PIN which consists of 4 bits not all the CRH Register */
-                            GPIOA_CRH &= ~ ((0b1111) << (Copy_PIN * 4));
+                            MGPIOA -> CRH &= ~ ((0b1111) << (Copy_PIN * 4));
                             /* Set the chosen MODE on the chosen PIN */
-                            GPIOA_CRH |=   ((Copy_MODE) << (Copy_PIN * 4));
+                            MGPIOA -> CRH |=   ((Copy_u8MODE) << (Copy_PIN * 4));
                         }
                     break;
 
@@ -48,9 +48,9 @@ void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , G
                             /*  Reset the chosen PIN which consists of 4 bits not all the CRL Register 
                                 As long as we will use logical operation we have to reset first "not to over-write"  */
                                 // Note : We can use 4 SET-PINss but it consumes more clocks        
-                            GPIOB_CRL &= ~ ((0b1111) << (Copy_PIN * 4));
+                            MGPIOB -> CRL &= ~ ((0b1111) << (Copy_PIN * 4));
                             /* Set the chosen MODE on the chosen PIN */
-                            GPIOB_CRL |=   ((Copy_MODE) << (Copy_PIN * 4));
+                            MGPIOB -> CRL |=   ((Copy_u8MODE) << (Copy_PIN * 4));
                         }
                     /* Check if Pin lies between 8-15 noting that each pin consists of 4 bits "CRH Configuration Register High" */
                     else if (Copy_PIN <= 15)
@@ -58,9 +58,9 @@ void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , G
                             /* To get the exact chosen Pin when it's multiplied by 4 */
                             Copy_PIN = Copy_PIN - 8;
                             /* Reset the chosen PIN which consists of 4 bits not all the CRH Register */
-                            GPIOB_CRH &= ~ ((0b1111) << (Copy_PIN * 4));
+                            MGPIOB -> CRH &= ~ ((0b1111) << (Copy_PIN * 4));
                             /* Set the chosen MODE on the chosen PIN */
-                            GPIOB_CRH |=   ((Copy_MODE) << (Copy_PIN * 4));
+                            MGPIOB -> CRH |=   ((Copy_u8MODE) << (Copy_PIN * 4));
                         }
                     break;
 
@@ -73,9 +73,9 @@ void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , G
                             /*  Reset the chosen PIN which consists of 4 bits not all the CRL Register 
                                 As long as we will use logical operation we have to reset first "not to over-write"  */
                                 // Note : We can use 4 SET-PINss but it consumes more clocks        
-                            GPIOC_CRL &= ~ ((0b1111) << (Copy_PIN * 4));
+                            MGPIOC -> CRL &= ~ ((0b1111) << (Copy_PIN * 4));
                             /* Set the chosen MODE on the chosen PIN */
-                            GPIOC_CRL |=   ((Copy_MODE) << (Copy_PIN * 4));
+                            MGPIOC -> CRL |=   ((Copy_u8MODE) << (Copy_PIN * 4));
                         }
                     /* Check if Pin lies between 8-15 "CRH Register" */
                     else if (Copy_PIN <= 15)
@@ -83,9 +83,9 @@ void MGPIO_voidSetPinDirection(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , G
                             /* To get the exact chosen Pin when it's multiplied by 4 */
                             Copy_PIN = Copy_PIN - 8;
                             /* Reset the chosen PIN which consists of 4 bits not all the CRH Register */
-                            GPIOC_CRH &= ~ ((0b1111) << (Copy_PIN * 4));
+                            MGPIOC -> CRH &= ~ ((0b1111) << (Copy_PIN * 4));
                             /* Set the chosen MODE on the chosen PIN */
-                            GPIOC_CRH |=   ((Copy_MODE) << (Copy_PIN * 4));
+                            MGPIOC -> CRH |=   ((Copy_u8MODE) << (Copy_PIN * 4));
                         }
                     break;
 
@@ -105,17 +105,17 @@ void MGPIO_voidSetPinValue(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , u8 Co
                     if (Copy_u8VALUE == HIGH) 
                         {
                                  /* Using ODR output data register GPIOA_ODR |=  (1 << (BIT)) 3 cycles */
-                            // SET_BIT(GPIOA_ODR , Copy_PIN);
+                            // SET_BIT(MGPIOA -> ODR , Copy_PIN);
                                  /* Using BSRR Bit Set Reset Register GPIOA_BSR = (1 << (BIT)) 2 cycles */
-                            GPIOA_BSR = (1 << Copy_PIN);
+                            MGPIOA -> BSR = (1 << Copy_PIN);
                         }
                     /* If the value entered by the user is low : clear the pin */
                     else if (Copy_u8VALUE == LOW)
                         {
                                  /* Using ODR output data Register GPIOA_ODR |=  (1 << (BIT)) 3 cycles */   
-                            // CLR_BIT(GPIOA_ODR , Copy_PIN);
+                            // CLR_BIT(MGPIOA -> ODR , Copy_PIN);
                                  /* Using BRR Bit Reset Register GPIOA_BRR = (1 << (BIT)) 2 cycles */
-                            GPIOA_BRR = (1 << Copy_PIN);
+                            MGPIOA -> BRR = (1 << Copy_PIN);
                         }
                     break;
 
@@ -124,17 +124,17 @@ void MGPIO_voidSetPinValue(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , u8 Co
                     if (Copy_u8VALUE == HIGH) 
                         {
                                  /* Using ODR output data register GPIOA_ODR |=  (1 << (BIT)) 3 cycles */
-                            //SET_BIT(GPIOB_ODR , Copy_PIN);
+                            //SET_BIT(MGPIOB -> ODR , Copy_PIN);
                                  /* Using BSRR Bit Set Reset Register GPIOA_BSR = (1 << (BIT)) 2 cycles */
-                            GPIOB_BSR = (1 << Copy_PIN);
+                            MGPIOB -> BSR = (1 << Copy_PIN);
                         }
                     /* If the value entered by the user is low : clear the pin */
                     else if (Copy_u8VALUE == LOW)
                         {   
                                  /* Using ODR output data register GPIOA_ODR |=  (1 << (BIT)) 3 cycles */    
-                            // CLR_BIT(GPIOB_ODR , Copy_PIN);
+                            // CLR_BIT(MGPIOB -> ODR , Copy_PIN);
                                  /* Using BRR Bit Reset Register GPIOA_BRR = (1 << (BIT)) 2 cycles */
-                            GPIOB_BRR = (1 << Copy_PIN);
+                            MGPIOB -> BRR = (1 << Copy_PIN);
                         }
                     break;
 
@@ -143,17 +143,17 @@ void MGPIO_voidSetPinValue(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , u8 Co
                     if (Copy_u8VALUE == HIGH) 
                         {
                                  /* Using ODR output data register GPIOA_ODR |=  (1 << (BIT)) 3 cycles */
-                            // SET_BIT(GPIOC_ODR , Copy_PIN);
+                            // SET_BIT(MGPIOC -> ODR , Copy_PIN);
                                  /* Using BSRR Bit Set Reset Register GPIOA_BSR = (1 << (BIT)) 2 cycles */
-                            GPIOC_BSR = (1 << Copy_PIN);
+                            MGPIOC -> BSR = (1 << Copy_PIN);
                         }
                     /* If the value entered by the user is low : clear the pin */
                     else if (Copy_u8VALUE == LOW)
                         {
                                  /* Using ODR output data register GPIOA_ODR |=  (1 << (BIT)) 3 cycles */ 
-                            // CLR_BIT(GPIOC_ODR , Copy_PIN);
+                            // CLR_BIT(MGPIOC -> ODR , Copy_PIN);
                                  /* Using BRR Bit Reset Register GPIOA_BRR = (1 << (BIT)) 2 cycles */
-                            GPIOC_BRR = (1 << Copy_PIN);
+                            MGPIOC -> BRR = (1 << Copy_PIN);
                         }
                     break;
 
@@ -163,20 +163,6 @@ void MGPIO_voidSetPinValue(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN , u8 Co
 
 }
 
-void MGPIO_voidTogglePinValue (GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN)
-{
-	switch (Copy_PORT) 
-    {
-		case GPIOA:
-			        TOG_BIT(GPIOA_ODR , Copy_PIN);     break;		                                    
-		case GPIOB:
-			        TOG_BIT(GPIOA_ODR , Copy_PIN);     break;		
-		case GPIOC: 
-			        TOG_BIT(GPIOC_ODR , Copy_PIN);     break;	
-		default   :                                    break;
-	}
-}
-
             /* ----- Function that get the pin value either "High or Low" ------- */
 u8 MGPIO_u8GetPinValue(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN)
 {
@@ -184,49 +170,131 @@ u8 MGPIO_u8GetPinValue(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN)
     switch (Copy_PORT)
     {
                     /* Get the pin value from the Input Data Register */
-        case GPIOA : LOC_u8Value = GET_BIT(GPIOA_IDR , Copy_PIN); break;
-        case GPIOB : LOC_u8Value = GET_BIT(GPIOB_IDR , Copy_PIN); break;
-        case GPIOC : LOC_u8Value = GET_BIT(GPIOC_IDR , Copy_PIN); break;
-        default    :                                              break;
+        case GPIOA : LOC_u8Value = GET_BIT(MGPIOA -> IDR , Copy_PIN); break;
+        case GPIOB : LOC_u8Value = GET_BIT(MGPIOB -> IDR , Copy_PIN); break;
+        case GPIOC : LOC_u8Value = GET_BIT(MGPIOC -> IDR , Copy_PIN); break;
+        default    :                                                  break;
     }
     return LOC_u8Value;
 }
 
-void GPIO_voidSetPortMode(GPIO_Port_ID Copy_PORT , GPIO_Modes Copy_MODE)
+                        /* ----- Function that toggle Pin ----- */
+void MGPIO_voidTogglePinValue (GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN)
 {
-	for (GPIO_Pin_No PIIN_S = PIN0 ; PIIN_S <= PIN15 ; PIIN_S ++)
+	switch (Copy_PORT) 
     {
-		MGPIO_voidSetPinDirection(Copy_PORT , PIIN_S , Copy_MODE);
+		case GPIOA:
+			        TOG_BIT(MGPIOA -> ODR , Copy_PIN);     break;		                                    
+		case GPIOB:
+			        TOG_BIT(MGPIOB -> ODR , Copy_PIN);     break;		
+		case GPIOC: 
+			        TOG_BIT(MGPIOC -> ODR , Copy_PIN);     break;	
+		default   :                                        break;
 	}
 }
 
+
+
+        /* ----- Function that Set mode of all port pins ----- */
+void MGPIO_voidSetPortMode(GPIO_Port_ID Copy_PORT , u8 Copy_u8MODE)
+{
+	for (GPIO_Pin_No PIIN_S = PIN0 ; PIIN_S <= PIN15 ; PIIN_S ++)
+    {
+		MGPIO_voidSetPinDirection(Copy_PORT , PIIN_S , Copy_u8MODE);
+	}
+}
+
+        /* ----- Function that Set direction of all port pins ----- */
+void MGPIO_VoidSetPortDirection(GPIO_Port_ID Copy_PORT , u8 Copy_u8Position , u8 Copy_u8MODE)
+{
+
+	switch(Copy_PORT)
+    {
+        case GPIOA:
+            if      (Copy_u8Position == LOW ) 
+                        { MGPIOA -> CRL = (0x11111111 * Copy_u8MODE) ; }
+            else if (Copy_u8Position == HIGH) 
+                        { MGPIOA -> CRH = (0x11111111 * Copy_u8MODE) ; }
+            break;
+
+        case GPIOB:
+            if      (Copy_u8Position == LOW ) 
+                        { MGPIOB -> CRL = (0x11111111 * Copy_u8MODE) ; }
+            else if (Copy_u8Position == HIGH) 
+                        { MGPIOB -> CRH = (0x11111111 * Copy_u8MODE) ; }
+            break;
+
+        case GPIOC:
+            if      ( Copy_u8Position == LOW  ) 
+                        { MGPIOC -> CRL = (0x11111111 * Copy_u8MODE) ; }
+            else if ( Copy_u8Position == HIGH ) 
+                        { MGPIOC -> CRH = (0x11111111 * Copy_u8MODE) ; }
+            break;
+	}
+}
+
+    /* ----- Function that Set value of all port pins ----- */
 void MGPIO_voidSetPortValue(GPIO_Port_ID Copy_PORT , u16 Copy_u16Value)
 {
 	switch (Copy_PORT) 
     {
 		case GPIOA :
-			GPIOA_ODR = Copy_u16Value;     break;
+			MGPIOA -> ODR = Copy_u16Value;     break;
 		case GPIOB :
-			GPIOB_ODR = Copy_u16Value;     break;
+			MGPIOB -> ODR = Copy_u16Value;     break;
 		case GPIOC : 
-			GPIOC_ODR = Copy_u16Value;     break;
-		default    :                       break;
+			MGPIOC -> ODR = Copy_u16Value;     break;
+		default    :                           break;
 	}
 }
 
-void GPIO_voidWritePins(GPIO_Port_ID Copy_PORT , GPIO_Pin_No CopyStartPinNumber , GPIO_No_Of_Pins CopyNumOfPins , u8 Copy_u8Value)
+/* ----- Function that get value of all port pins ----- */
+u16 MGPIO_u16GetPortValue(GPIO_Port_ID Copy_PORT , u8 Copy_u8Position )
+{
+	u16 LOC_u16Result = 0;
+
+	switch(Copy_PORT)
+    {
+        case GPIOA:
+            LOC_u16Result = MGPIOA -> IDR;
+            if      (Copy_u8Position == LOW )
+                        { LOC_u16Result &= 0x00FF ; }
+            else if (Copy_u8Position == HIGH)
+                        { LOC_u16Result &= 0xFF00 ; }
+            break;
+
+        case GPIOB:
+            LOC_u16Result = MGPIOB -> IDR;
+            if      ( Copy_u8Position == LOW  )
+                        { LOC_u16Result &= 0x00FF ; }
+            else if ( Copy_u8Position == HIGH )
+                        { LOC_u16Result &= 0xFF00 ; }
+            break;
+
+        case GPIOC:
+            LOC_u16Result = MGPIOC -> IDR;
+            if      ( Copy_u8Position == LOW  )
+                        { LOC_u16Result &= 0x00FF ; }
+            else if ( Copy_u8Position == HIGH )
+                        { LOC_u16Result &= 0xFF00 ; }
+            break;
+    }
+	return LOC_u16Result ;
+}
+
+void MGPIO_voidWritePins(GPIO_Port_ID Copy_PORT , GPIO_Pin_No CopyStartPinNumber , GPIO_No_Of_Pins CopyNumOfPins , u8 Copy_u8Value)
 {
 	switch (Copy_PORT) 
     {
 		case GPIOA :
-			GPIOA_ODR &= ~ (CopyNumOfPins << CopyStartPinNumber );
-			GPIOA_ODR |=   (Copy_u8Value  << CopyStartPinNumber );          break;
+			MGPIOA -> ODR &= ~ (CopyNumOfPins << CopyStartPinNumber );
+			MGPIOA -> ODR |=   (Copy_u8Value  << CopyStartPinNumber );          break;
 		case GPIOB :
-			GPIOB_ODR &= ~ (CopyNumOfPins << CopyStartPinNumber );
-			GPIOB_ODR |=   (Copy_u8Value  << CopyStartPinNumber );          break;
+			MGPIOB -> ODR &= ~ (CopyNumOfPins << CopyStartPinNumber );
+			MGPIOB -> ODR |=   (Copy_u8Value  << CopyStartPinNumber );          break;
 		case GPIOC : 
-			GPIOC_ODR &= ~ (CopyNumOfPins << CopyStartPinNumber );
-			GPIOC_ODR |=   (Copy_u8Value  << CopyStartPinNumber );          break;
+			MGPIOC -> ODR &= ~ (CopyNumOfPins << CopyStartPinNumber );
+			MGPIOC -> ODR |=   (Copy_u8Value  << CopyStartPinNumber );          break;
 		default    :                                                        break;
 	}
 }
@@ -240,46 +308,45 @@ void MGPIO_u8LockPin(GPIO_Port_ID Copy_PORT , GPIO_Pin_No Copy_PIN)
 		case GPIOA :
 			Temp |=  (1 << Copy_PIN);
 			/* Set LCKK bit */
-			GPIOA_LCK = Temp;
+			MGPIOA -> LCK = Temp;
 			/* Reset LCKK bit */
-			GPIOA_LCK =  (1 << Copy_PIN);
+			MGPIOA -> LCK =  (1 << Copy_PIN);
 			/* Set LCKK bit */
-			GPIOA_LCK = Temp;
+			MGPIOA -> LCK = Temp;
 			/* Read LCKK bit*/
-			Temp = GPIOA_LCK;
+			Temp = MGPIOA -> LCK;
 			/* Read LCKK bit*/
-			Temp = GPIOA_LCK;
+			Temp = MGPIOA -> LCK;
 			break;
 
 		case GPIOB :
 			Temp |=  (1 << Copy_PIN) ;
 			/* Set LCKK bit */
-			GPIOB_LCK = Temp;
+			MGPIOB -> LCK = Temp;
 			/* Reset LCKK bit */
-			GPIOB_LCK =  (1 << Copy_PIN);
+			MGPIOB -> LCK =  (1 << Copy_PIN);
 			/* Set LCKK bit */
-			GPIOB_LCK = Temp;
+			MGPIOB -> LCK = Temp;
 			/* Read LCKK bit*/
-			Temp = GPIOB_LCK;
+			Temp = MGPIOB -> LCK;
 			/* Read LCKK bit*/
-			Temp = GPIOB_LCK;
+			Temp = MGPIOB -> LCK;
 			break;
 
 		case GPIOC : 
 			Temp |= (1 << Copy_PIN);
 			/* Set LCKK bit */
-			GPIOC_LCK = Temp;
+			MGPIOC -> LCK = Temp;
 			/* Reset LCKK bit */
-			GPIOC_LCK = (1 << Copy_PIN);
+			MGPIOC -> LCK = (1 << Copy_PIN);
 			/* Set LCKK bit */
-			GPIOC_LCK = Temp;
+			MGPIOC -> LCK = Temp;
 			/* Read LCKK bit*/
-			Temp = GPIOC_LCK;
+			Temp = MGPIOC -> LCK;
 			/* Read LCKK bit*/
-			Temp = GPIOC_LCK;
+			Temp = MGPIOC -> LCK;
 			break;
 
-		default    : 
-            break;
+		default:                    break;
 	}
 }
