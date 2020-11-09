e572afca1c2ef3460489a14493497ee982675081
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-09 22:13:09 +0200

    Update STK_private.h

--- STM32F103C8T6_ARM Cortex M3/SysTick/STK_private.h
+++ STM32F103C8T6_ARM Cortex M3/SysTick/STK_private.h
@@ -2,7 +2,7 @@
 /* Author : AbdElrahman I.Zaki                                                    */
 /* Date   : 26 August 2020                                                        */
 /* Version: V01                                                                   */
-/* Desc.  : STM32F103C8T6 ARM Cortex M3 Systick Prototypes of private Functions   */
+/* Desc.  : STM32F103C8T6 ARM Cortex M3 Systick Registers Defintions              */
 /**********************************************************************************/
 
 #ifndef STK_PRIVATE_H
@@ -16,24 +16,11 @@ typedef struct
     volatile u32 STK_LOAD;
     /* Systick current value register */
     volatile u32 STK_VAL;
+    /* SysTick calibration value register */
+    volatile u32 STK_CALIB;
 }MSTK_Type;
 
 #define MSTK_Ptr    ((MSTK_Type *) 0xE000E010) // Pointer to struct
-
-/* -------- CLK Options -------- */
-#define AHB_CLK_SRC           0
-#define AHB_CLK_SRC_Div_8     1    
-
-/* -------- Interrupt Options -------- */
-#define ENABLE           1
-#define DISABLE          0
-
-/* -------- Bits -------- */
-#define STK_CTRL_ENABLE       0
-#define STK_CTRL_TICKINT      1
-#define STK_CTRL_CLKSOURCE    2
-#define STK_CTRL_COUNTFLAG    16
-
 
 
 
