da39743160bce597a592db50f648116eecc6caf6
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-09 22:14:49 +0200

    Update RCC_program.c

--- STM32F103C8T6_ARM Cortex M3/RCC/RCC_program.c
+++ STM32F103C8T6_ARM Cortex M3/RCC/RCC_program.c
@@ -12,238 +12,166 @@
 #include "RCC_private.h"
 #include "RCC_config.h"
 
-/* Initialize clock depending on paramters chosen by the user */
-void RCC_voidInitSysClock (void)
+
+/* ----- Initialize clock depending on paramters chosen by the user ----- */
+void MRCC_voidInitSysClock (void)
 {
+	u32 LOC_u32TimeOut = 0;
+
 	/* Choose CLK System from (HSI - HSE Crystal - HSE RC - PLL) */
-	#if SYS_CLOCK == RCC_HSI 
-		SET_BIT(RCC_CR   , RCC_HSI_ON);         // HSI On @ CR 
-		while( !(RCC_CR & (1<<RCC_HSI_RDY) ) ); // Wait Till HSI RDY
-		CLR_BIT(RCC_CFGR , RCC_SW0);            // Choosing HSI from System Clock Switch @ CFGR
-		CLR_BIT(RCC_CFGR , RCC_SW1);
-		
-	#elif SYS_CLOCK == RCC_HSE_CRYSTAL
-		SET_BIT(RCC_CR   , RCC_HSE_ON);         // HSE On @ CR 
-		while( !(RCC_CR & (1<<RCC_HSE_RDY) ) ); // Wait Till HSE RDY
-		CLR_BIT(RCC_CR   , RCC_HSE_BYP);	    // Disable bypass
-		CLR_BIT(RCC_CFGR , RCC_SW1 );           // Choosing HSE from System Clock Switch @ CFGR
-		SET_BIT(RCC_CFGR , RCC_SW0 );
+	#if RCC_SYS_CLOCK == RCC_HSI 	
+		/* BIT 1:0 -> Choose Between HSI OR HSE OR PLL */
+		/* HSI Selected As A System Clock */
+		CLR_BIT(MRCC_Ptr -> CFGR, 0);
+		CLR_BIT(MRCC_Ptr -> CFGR, 1);
+		/* Bit 0 -> Enable The HSI Clock */
+		SET_BIT(MRCC_Ptr -> CR, 0);
+		/* Bit 1 -> Checking While The HSI Clock Is Stable */
+		while( ( ( GET_BIT( MRCC_Ptr->CR , 1 ) ) == 0 ) && ( LOC_u32TimeOut < 100000 ) ){ LOC_u32TimeOut++; }
+		if(LOC_u32TimeOut >= 100000){ /*TimeOut*/ }	
+
+	#elif RCC_SYS_CLOCK == RCC_HSE_CRYSTAL /* HSE not bypassed */
+		/* BIT 1:0 -> Choose Between HSI OR HSE OR PLL */
+		/* HSE Selected As A System Clock */
+		SET_BIT(MRCC_Ptr -> CFGR, 0);
+		CLR_BIT(MRCC_Ptr-> CFGR, 1);
+		/* Bit 16 -> Enable The HSE Clock */
+		SET_BIT(MRCC_Ptr -> CR, 16);
+		/* Bit 17 -> Checking While The HSE Clock Is Stable */
+		while( ( ( GET_BIT(MRCC_Ptr -> CR , 17) ) == 0 ) && ( LOC_u32TimeOut < 100000 ) ){ LOC_u32TimeOut++; }
+		if(LOC_u32TimeOut >= 100000){ /*TimeOut*/ }
 	
-	#elif SYS_CLOCK == RCC_HSE_RC
-		SET_BIT(RCC_CR   , RCC_HSE_ON);          // HSE On @ CR 
-		while( !(RCC_CR & (1<<RCC_HSE_RDY) ) );  // Wait Till HSE RDY
-		SET_BIT(RCC_CR   , RCC_HSE_BYP);	     // Enable bypass
-		CLR_BIT(RCC_CFGR , RCC_SW1);             // Choosing HSE from System Clock Switch @ CFGR
-		SET_BIT(RCC_CFGR , RCC_SW0);
+	#elif RCC_SYS_CLOCK == RCC_HSE_RC /* HSE bypassed */
+		/* The HSEBYP Can Be Written Only When HSE Oscilator Is Disabled */
+		/* HSE Clock Disabled */
+		CLR_BIT(MRCC_Ptr -> CR, 16);
+		/* BIT 18 -> To Select HSE BYPASS */
+		/* HSEBYPASS Clock Enable */
+		SET_BIT(MRCC_Ptr -> CR, 18);
+		/* BIT 1:0 -> Choose Between HSI OR HSE OR PLL */
+		/* HSE Selected As A System Clock */
+		SET_BIT(MRCC_Ptr -> CFGR, 0);
+		CLR_BIT(MRCC_Ptr -> CFGR, 1);
+		/* Bit 16 -> Enable The HSE Clock */
+		SET_BIT(MRCC_Ptr -> CR, 16);
+		/* Bit 17 -> Checking While The HSE Clock Is Stable */
+		while( ( ( GET_BIT( MRCC_Ptr -> CR , 17) ) == 0 ) && ( LOC_u32TimeOut < 100000 ) ){ LOC_u32TimeOut++; }
+		if(LOC_u32TimeOut >= 100000){ /*TimeOut*/ }
+
+    #elif RCC_SYS_CLOCK == RCC_PLL
 		
-    #elif SYS_CLOCK == PLL_CLOCK
-		
-		RCC_CFGR &= ~(15 << RCC_PLL_MUL0);                 // Clear the four bits 18-21 @ CFGR
-		RCC_CFGR |= ((RCC_PLL_MUL - 2) << RCC_PLL_MUL0);   // Load the new multiplication value chosen by user
-		
-		    #if   RCC_PLL_SOURCE == RCC_PLL_HSI_BY2
-			    SET_BIT(RCC_CR   , RCC_HSI_ON );      // HSI on
-			    CLR_BIT(RCC_CFGR , RCC_PLL_SRC);      // CLR PLL SRC Bit to Select HSI/2
-			
-		    #elif RCC_PLL_SOURCE == RCC_PLL_HSE
-			    SET_BIT(RCC_CR   , RCC_HSE_ON );      // HSE on
-			    SET_BIT(RCC_CFGR , RCC_PLL_SRC);      // SET PLL SRC Bit HSE to Select HSE
-			
-		    #elif RCC_PLL_SOURCE == RCC_PLL_HSE_BY2 
-			    SET_BIT(RCC_CR   , RCC_HSE_ON   );    // HSE on
-			    SET_BIT(RCC_CFGR , RCC_PLL_XTPRE);    // Enable prescaller by 2
-			    SET_BIT(RCC_CFGR , RCC_PLL_SRC  );    // SET PLL SRC Bit HSE to Select HSE
-			
-		    #else
-			    #error "PLL Configuration error"
-		    #endif
-	
-		while( !(RCC_CR & (1<<RCC_PLL_RDY) ) );  // Wait till PLL RDY
-		SET_BIT(RCC_CR   , RCC_PLL_ON);          // PLL On
-		CLR_BIT(RCC_CFGR , RCC_SW0   );          // Choosing PLL from System Clock Switch @ CFGR
-		SET_BIT(RCC_CFGR , RCC_SW1   );           
+		/* BIT 1:0 -> Choose Between HSI OR HSE OR PLL */
+		/* PLL Selected As A System Clock */
+		CLR_BIT(MRCC_Ptr -> CFGR, 0);
+		SET_BIT(MRCC_Ptr -> CFGR, 1);
+		/*The Start Of Nested #IF*/
+		/* Choosing The Multiplication Factor For PLL */
+		#if (CLK_FACTOR >= NO_CLOCK_FACTOR) && (CLK_FACTOR <= PLL_CLOCK_MULTIPLE_BY_16) 
+			MRCC_Ptr -> CFGR &= ~( ( 0b1111 )     << 18 ) ;
+			MRCC_Ptr -> CFGR |=  (CLK_FACTOR) << 18   ;
+		#elif (CLK_FACTOR > PLL_CLOCK_MULTIPLE_BY_16) || (CLK_FACTOR < NO_CLOCK_FACTOR)
+			#warning "Wrong Multiplication Factor"
+		/*The End OF Nested #IF */
+		#endif
+
+		/*The Start Of Nested #IF*/
+		#if RCC_PLL_SOURCE == PLL_HSI_DIVIDED_BY_2
+
+			/* BIT 16 -> Choose PLL Source -> If HSI/2 OR HSE */
+			/* PLL Entery Clock Source Is HSI Divided By 2 */
+			CLR_BIT(MRCC_Ptr -> CFGR, 16);
+
+		#elif RCC_PLL_SOURCE == PLL_HSE
+			/* Bit 16 -> Enable The HSE Clock */
+			SET_BIT(MRCC_Ptr -> CR, 16);
+			/* BIT 16 -> Choose PLL Source -> If HSI/2 OR HSE */
+			/* PLL Entery Clock Source Is HSE */
+			SET_BIT(MRCC_Ptr -> CFGR, 16);
+			/* BIT 17 -> IF PLL Source IS HSE Then Choose Between Divide HSE/2 Or Not  */
+			/* HSE Is Not Divided */
+			CLR_BIT(MRCC_Ptr -> CFGR, 17);
+
+		#elif RCC_PLL_SOURCE == PLL_HSE_DIVIDED_BY_2
+			/* Bit 16 -> Enable The HSE Clock */
+			SET_BIT(MRCC_Ptr -> CR, 16);
+			/* BIT 16 -> Choose PLL Source -> If HSI/2 OR HSE */
+			/* PLL Entery Clock Source Is HSE */
+			SET_BIT(MRCC_Ptr -> FGR, 16);
+			/* BIT 17 -> IF PLL Source IS HSE Then Choose Between Divide HSE/2 Or Not  */
+			/* HSE Is Divided By Two */
+			SET_BIT(MRCC_Ptr -> CFGR, 17);
+
+			/*The End Of Nested IF*/
+		#endif
+
+		/* PLL Clock Enable */
+		SET_BIT(MRCC_Ptr -> CR, 24);
+
+		/* Wait Until PLL Ready Flag Set */
+		while( ( (GET_BIT(MRCC_Ptr -> CR, 25) ) == 0 ) && (LOC_u32TimeOut < 100000) ){ LOC_u32TimeOut++; }
+		if(LOC_u32TimeOut >= 100000){ /*TimeOut*/ }  
 
 	#else
-		#error "CLK System Configuration error"
-	#endif
-	
-
-	
-	/* Set the correction factor */ 
-	#if HSI_STEP_CORRECTION != 0 
-		RCC_CR &= ~(31 << RCC_HSI_TRIM0);      // Clear the five bits
-		RCC_CR |= ((HSI_STEP_CORRECTION + 16 ) << RCC_HSI_TRIM0);
+		#error "Invalid CLK System Configuration"
 	#endif
 
 
-
-	
-	/* Choosing output on MCO  Control Register Bits: 26-24*/
-	#if   RCC_MCO_PIN == NO_CLOCK 
-		CLR_BIT(RCC_CFGR , RCC_MCO_2);  
-		
-	#elif RCC_MCO_PIN == HSI_CLOCK
-		SET_BIT(RCC_CR   , RCC_HSI_ON);  // HSI on
-		SET_BIT(RCC_CFGR , RCC_MCO_0 );
-		CLR_BIT(RCC_CFGR , RCC_MCO_1 );
-		SET_BIT(RCC_CFGR , RCC_MCO_2 );
-		
-    #elif RCC_MCO_PIN == HSE_CLOCK
-		SET_BIT(RCC_CR   , RCC_HSE_ON);  // HSE on
-		CLR_BIT(RCC_CFGR , RCC_MCO_0 );
-		SET_BIT(RCC_CFGR , RCC_MCO_1 );
-		SET_BIT(RCC_CFGR , RCC_MCO_2 );
-		
-	#elif RCC_MCO_PIN == PLL_CLOCK
-        SET_BIT(RCC_CR   , RCC_PLL_ON);  // PLL on
-		SET_BIT(RCC_CFGR , RCC_MCO_0 );
-		SET_BIT(RCC_CFGR , RCC_MCO_1 );
-		SET_BIT(RCC_CFGR , RCC_MCO_2 );
-		
-	#elif RCC_MCO_PIN == SYSTEM_CLOCK
-		CLR_BIT(RCC_CFGR , RCC_MCO_0);
-		CLR_BIT(RCC_CFGR , RCC_MCO_1);
-		SET_BIT(RCC_CFGR , RCC_MCO_2);
-	#else
-		#error "MCO Configuration error"
-	#endif
-	
-	
-	
-	/* To enable or disable Clock security system */
-	#if CSS_ON == ENABLE 
-		SET_BIT(RCC_CR , RCC_CSS_ON);
-	#elif CSS_ON == DISABLE
-		CLR_BIT(RCC_CR , RCC_CSS_ON);
+	/* ----- To enable or disable Clock security system ----- */
+	#if CLK_SECURITY_SYSTEM == ENABLE_CLOCK_SECURITY_SYSTEM 
+		SET_BIT(MRCC_Ptr -> CR, 19);
+	#elif CLK_SECURITY_SYSTEM == DISABLE_CLOCK_SECURITY_SYSTEM
+		CLR_BIT(MRCC_Ptr -> CR, 19);
     #else
 		#error "CSS Configuration error"
 	#endif
-	
 
-	/* Choosing a prescaler for the ADC */
-	#if   RCC_ADC_PRESCALER == RCC_PRESCALER_2 
-		CLR_BIT(RCC_CFGR , RCC_ADCPRE0);
-		CLR_BIT(RCC_CFGR , RCC_ADCPRE2);
-	#elif RCC_ADC_PRESCALER == RCC_PRESCALER_4
-		SET_BIT(RCC_CFGR , RCC_ADCPRE0);
-		CLR_BIT(RCC_CFGR , RCC_ADCPRE2);
-	#elif RCC_ADC_PRESCALER == RCC_PRESCALER_6
-		CLR_BIT(RCC_CFGR , RCC_ADCPRE0);
-		SET_BIT(RCC_CFGR , RCC_ADCPRE2);
-	#elif RCC_ADC_PRESCALER == RCC_PRESCALER_8
-		SET_BIT(RCC_CFGR , RCC_ADCPRE0);
-		SET_BIT(RCC_CFGR , RCC_ADCPRE2);
-	#else
-		#error "ADC Prescaler Configuration error")
-	#endif
-	
 
-	/* Choosing a prescaler for the APB2 */
-	#if   RCC_APB2_PRESCALER == RCC_PRESCALER_0 
-		CLR_BIT(RCC_CFGR , RCC_PPRE2_2);
-	#elif RCC_APB2_PRESCALER == RCC_PRESCALER_2
-		CLR_BIT(RCC_CFGR , RCC_PPRE2_0);
-		CLR_BIT(RCC_CFGR , RCC_PPRE2_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE2_2);
-	#elif RCC_APB2_PRESCALER == RCC_PRESCALER_4
-		SET_BIT(RCC_CFGR , RCC_PPRE2_0);
-		CLR_BIT(RCC_CFGR , RCC_PPRE2_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE2_2);
-	#elif RCC_APB2_PRESCALER == RCC_PRESCALER_8
-		CLR_BIT(RCC_CFGR , RCC_PPRE2_0);
-		SET_BIT(RCC_CFGR , RCC_PPRE2_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE2_2);
-	#elif RCC_APB2_PRESCALER == RCC_PRESCALER_16
-		SET_BIT(RCC_CFGR , RCC_PPRE2_0);
-		SET_BIT(RCC_CFGR , RCC_PPRE2_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE2_2);
+	/* ----- Choosing output on MCO  Control Register Bits: 26-24 ----- */
+	#if   RCC_MCO_PIN == NO_CLOCK 
+		CLR_BIT(MRCC_Ptr -> CFGR , 26);
+		CLR_BIT(MRCC_Ptr -> CFGR , 27);  
+		
+	#elif RCC_MCO_PIN == HSI_CLOCK
+		SET_BIT(MRCC_Ptr -> CR   , 0);  // HSI on
+		SET_BIT(MRCC_Ptr -> CFGR , 24);
+		CLR_BIT(MRCC_Ptr -> CFGR , 25);
+		SET_BIT(MRCC_Ptr -> CFGR , 26);
+		CLR_BIT(MRCC_Ptr -> CFGR , 27);
+		
+    #elif RCC_MCO_PIN == HSE_CLOCK
+		SET_BIT(MRCC_Ptr -> CR   , 16);  // HSE on
+		CLR_BIT(MRCC_Ptr -> CFGR , 24);
+		SET_BIT(MRCC_Ptr -> CFGR , 25);
+		SET_BIT(MRCC_Ptr -> CFGR , 26);
+		CLR_BIT(MRCC_Ptr -> CFGR , 27);
+		
+	#elif RCC_MCO_PIN == PLL_CLOCK_BY2
+        SET_BIT(MRCC_Ptr -> CR   , 24);  // PLL on
+		SET_BIT(MRCC_Ptr -> CFGR , 24);
+		SET_BIT(MRCC_Ptr -> CFGR , 25);
+		SET_BIT(MRCC_Ptr -> CFGR , 26);
+		CLR_BIT(MRCC_Ptr -> CFGR , 27);
+		
+	#elif RCC_MCO_PIN == SYSTEM_CLOCK
+		CLR_BIT(MRCC_Ptr -> CFGR , 24);
+		CLR_BIT(MRCC_Ptr -> CFGR , 25);
+		SET_BIT(MRCC_Ptr -> CFGR , 26);
 	#else
-		#error "APB2 Prescaler Configuration error"
-	#endif
-	
-	/* Choosing a prescaler for the APB1 */
-	#if   RCC_APB1_PRESCALER == RCC_PRESCALER_0 
-		CLR_BIT(RCC_CFGR , RCC_PPRE1_2);
-	#elif RCC_APB1_PRESCALER == RCC_PRESCALER_2
-		CLR_BIT(RCC_CFGR , RCC_PPRE1_0);
-		CLR_BIT(RCC_CFGR , RCC_PPRE1_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE1_2);
-	#elif RCC_APB1_PRESCALER == RCC_PRESCALER_4
-		SET_BIT(RCC_CFGR , RCC_PPRE1_0);
-		CLR_BIT(RCC_CFGR , RCC_PPRE1_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE1_2);
-	#elif RCC_APB1_PRESCALER == RCC_PRESCALER_8
-		CLR_BIT(RCC_CFGR , RCC_PPRE1_0);
-		SET_BIT(RCC_CFGR , RCC_PPRE1_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE1_2);
-	#elif RCC_APB1_PRESCALER == RCC_PRESCALER_16
-		SET_BIT(RCC_CFGR , RCC_PPRE1_0);
-		SET_BIT(RCC_CFGR , RCC_PPRE1_1);
-		SET_BIT(RCC_CFGR , RCC_PPRE1_2);
-	#else
-		#error "APB1 Configuration error"
-	#endif
-	
-	/* Choosing a prescaller for the AHB */
-	#if   RCC_AHB_PRESCALER == RCC_PRESCALER_0 
-		CLR_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_2
-		CLR_BIT(RCC_CFGR , RCC_HPRE0);
-		CLR_BIT(RCC_CFGR , RCC_HPRE1);
-		CLR_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_4
-		SET_BIT(RCC_CFGR , RCC_HPRE0);
-		CLR_BIT(RCC_CFGR , RCC_HPRE1);
-		CLR_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_8
-		CLR_BIT(RCC_CFGR , RCC_HPRE0);
-		SET_BIT(RCC_CFGR , RCC_HPRE15);
-		CLR_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_16
-		SET_BIT(RCC_CFGR , RCC_HPRE0);
-		SET_BIT(RCC_CFGR , RCC_HPRE1);
-		CLR_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_64
-		CLR_BIT(RCC_CFGR , RCC_HPRE0);
-		CLR_BIT(RCC_CFGR , RCC_HPRE1);
-		SET_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_128
-		SET_BIT(RCC_CFGR , RCC_HPRE0);
-		CLR_BIT(RCC_CFGR , RCC_HPRE1);
-		SET_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_256
-		CLR_BIT(RCC_CFGR , RCC_HPRE0);
-		SET_BIT(RCC_CFGR , RCC_HPRE1);
-		SET_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#elif RCC_AHB_PRESCALER == RCC_PRESCALER_512
-		SET_BIT(RCC_CFGR , RCC_HPRE0);
-		SET_BIT(RCC_CFGR , RCC_HPRE1);
-		SET_BIT(RCC_CFGR , RCC_HPRE2);
-		SET_BIT(RCC_CFGR , RCC_HPRE3);
-	#else
-		#error "AHB Prescaler Configuration error"
-	#endif
-	
+		#error "Invalid MCO Configuration"
+	#endif	
 }
 
 
-void RCC_voidEnableClock (u8 Copy_u8BusId , u8 Copy_u8PerId)
+void MRCC_voidEnableClock (u8 Copy_u8BusId , u8 Copy_u8PerId)
 {
     if (Copy_u8PerId <= 31)
     {
       switch (Copy_u8BusId) 
       {
-        case RCC_AHP  : SET_BIT (RCC_AHBENR  , Copy_u8PerId ); break;
-        case RCC_APB1 : SET_BIT (RCC_APB1ENR , Copy_u8PerId ); break;
-        case RCC_APB2 : SET_BIT (RCC_APB2ENR , Copy_u8PerId ); break;
-        default       :                                        break;
+        case AHB_BUS  : SET_BIT(MRCC_Ptr -> AHBENR  , Copy_u8PerId ); break;
+        case APB1_BUS : SET_BIT(MRCC_Ptr -> APB1ENR , Copy_u8PerId ); break;
+        case APB2_BUS : SET_BIT(MRCC_Ptr -> APB2ENR , Copy_u8PerId ); break;
+        default       :                                      	      break;
       }
     }
     else 
@@ -252,16 +180,16 @@ void RCC_voidEnableClock (u8 Copy_u8BusId , u8 Copy_u8PerId)
     }
 }
 
-void RCC_voidDisableClock(u8 Copy_u8BusId , u8 Copy_u8PerId)
+void MRCC_voidDisableClock(u8 Copy_u8BusId , u8 Copy_u8PerId)
 {
     if (Copy_u8PerId <= 31)
     {
       switch (Copy_u8BusId) 
       {
-        case RCC_AHP  : CLEAR_BIT (RCC_AHBENR  , Copy_u8PerId ); break;
-        case RCC_APB1 : CLEAR_BIT (RCC_APB1ENR , Copy_u8PerId ); break;
-        case RCC_APB2 : CLEAR_BIT (RCC_APB2ENR , Copy_u8PerId ); break;
-        default       :                                          break;
+        case AHB_BUS  : CLR_BIT(MRCC_Ptr -> AHBENR  , Copy_u8PerId ); break;
+        case APB1_BUS : CLR_BIT(MRCC_Ptr -> APB1ENR , Copy_u8PerId ); break;
+        case APB2_BUS : CLR_BIT(MRCC_Ptr -> APB2ENR , Copy_u8PerId ); break;
+        default       :                                  		      break;
       }
     }
     else 
