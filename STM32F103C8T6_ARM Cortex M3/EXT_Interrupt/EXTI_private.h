814a2204472cf1bf7a1054aed6fb0c4d479d006d
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-09 22:22:53 +0200

    Update EXTI_private.h

--- STM32F103C8T6_ARM Cortex M3/EXT_Interrupt/EXTI_private.h
+++ STM32F103C8T6_ARM Cortex M3/EXT_Interrupt/EXTI_private.h
@@ -5,14 +5,15 @@
 /* Desc.  : STM32F103C8T6 ARM Cortex M3 EXT Int Prototypes of private Functions   */
 /**********************************************************************************/
 
-#ifndef EXTI_PRIVATE_H
-#define EXTI_PRIVATE_H
+#ifndef _EXTI_PRIVATE_H_
+#define _EXTI_PRIVATE_H_
 
 /* Typedef define a new data type based on an old data type  typdef  unsigned char   u8
                                                                         old          new 
     in struct EXTI_Type is the new data type and struct with all its content is old data type */
 
-typedef stuct{
+typedef struct
+{
 
      volatile u32 EXTI_IMR;          // Interrupt Mask Register               -> Interrupt on Line x, Enable 1/Disable 0
      volatile u32 EXTI_EMR;          // Event Mask Register                   -> 
@@ -28,29 +29,7 @@ typedef stuct{
     and the first register offset noting that each step is by 32bits */
 #define EXTI_Ptr    ((volatile EXTI_Type*) 0x40010400) 
 
-/* EXTI16 -> PVD , EXTI17 -> RTC , EXTI18 -> USB , EXTI19 -> Ethernet wakeup */
-/* Line0 = GPIOA0 GPIOB0 GPIOC0 and so on */
-#define LINE0       0
-#define LINE1       1
-#define LINE2       2
-#define LINE3       3
-#define LINE4       4
-#define LINE5       5
-#define LINE6       6
-#define LINE7       7
-#define LINE8       8
-#define LINE9       9
-#define LINE10      10
-#define LINE11      11
-#define LINE12      12
-#define LINE13      13
-#define LINE14      14
-#define LINE15      15
 
-
-#define RISING_EDGE         0
-#define FALLING_EDGE        1
-#define ON_CHANGE           2
 
 
 
