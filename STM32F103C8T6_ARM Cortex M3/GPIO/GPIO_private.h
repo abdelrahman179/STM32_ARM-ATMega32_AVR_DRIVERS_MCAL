775287e75e17215b88e0b831961175d084fd868d
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-09 22:17:17 +0200

    Update GPIO_private.h

--- STM32F103C8T6_ARM Cortex M3/GPIO/GPIO_private.h
+++ STM32F103C8T6_ARM Cortex M3/GPIO/GPIO_private.h
@@ -1,44 +1,30 @@
 /****************************************************************************************/
 /* Author : AbdElrahman I.Zaki                                                          */
 /* Date   : 10 August 2020                                                              */
-/* Version: V01                                                                         */
+/* Version: V02                                                                         */
 /* Desc.  : STM32F103C8T6 ARM Cortex M3 GPIO Register addresses and private functions   */
 /****************************************************************************************/
 
-#ifndef DIO_PRIVATE_H
-#define DIO_PRIVATE_H
+#ifndef _GDIO_PRIVATE_H_
+#define _GDIO_PRIVATE_H_
 
-/* GPIO Ports Base Addresses */
-#define GPIOA_BASE_ADDRESS      0x40010800
-#define GPIOB_BASE_ADDRESS      0x40010C00
-#define GPIOC_BASE_ADDRESS      0x40011000
 
-/* GPIO PORTA Registers Addresses "Base-address + Offset" */
-#define GPIOA_CRL       *((volatile u32 *)(GPIOA_BASE_ADDRESS + 0x00))
-#define GPIOA_CRH       *((volatile u32 *)(GPIOA_BASE_ADDRESS + 0x04))
-#define GPIOA_IDR       *((volatile u32 *)(GPIOA_BASE_ADDRESS + 0x08))
-#define GPIOA_ODR       *((volatile u32 *)(GPIOA_BASE_ADDRESS + 0x0C))
-#define GPIOA_BSR       *((volatile u32 *)(GPIOA_BASE_ADDRESS + 0x10))
-#define GPIOA_BRR       *((volatile u32 *)(GPIOA_BASE_ADDRESS + 0x14))
-#define GPIOA_LCK       *((volatile u32 *)(GPIOA_BASE_ADDRESS + 0x18))
 
-/* GPIO PORTB Registers Addresses "Base-address + Offset" */
-#define GPIOB_CRL       *((volatile u32 *)(GPIOB_BASE_ADDRESS + 0x00))
-#define GPIOB_CRH       *((volatile u32 *)(GPIOB_BASE_ADDRESS + 0x04))
-#define GPIOB_IDR       *((volatile u32 *)(GPIOB_BASE_ADDRESS + 0x08))
-#define GPIOB_ODR       *((volatile u32 *)(GPIOB_BASE_ADDRESS + 0x0C))
-#define GPIOB_BSR       *((volatile u32 *)(GPIOB_BASE_ADDRESS + 0x10))
-#define GPIOB_BRR       *((volatile u32 *)(GPIOB_BASE_ADDRESS + 0x14))
-#define GPIOB_LCK       *((volatile u32 *)(GPIOB_BASE_ADDRESS + 0x18))
+typedef struct 
+{
+    volatile u32 CRL; // Port configuration register low Pins  0 - 15
+    volatile u32 CRH; // Port configuration register high Pins 16 - 31
+    volatile u32 IDR; // Port input data register
+    volatile u32 ODR; // Port output data register
+    volatile u32 BSR; // Port bit set/reset register 
+    volatile u32 BRR; // Port bit reset register
+    volatile u32 LCK; // Port configuration lock register
+}GPIO;
 
-/* GPIO PORTC Registers Addresses "Base-address + Offset" */
-#define GPIOC_CRL       *((volatile u32 *)(GPIOC_BASE_ADDRESS + 0x00))
-#define GPIOC_CRH       *((volatile u32 *)(GPIOC_BASE_ADDRESS + 0x04))
-#define GPIOC_IDR       *((volatile u32 *)(GPIOC_BASE_ADDRESS + 0x08))
-#define GPIOC_ODR       *((volatile u32 *)(GPIOC_BASE_ADDRESS + 0x0C))
-#define GPIOC_BSR       *((volatile u32 *)(GPIOC_BASE_ADDRESS + 0x10))
-#define GPIOC_BRR       *((volatile u32 *)(GPIOC_BASE_ADDRESS + 0x14))
-#define GPIOC_LCK       *((volatile u32 *)(GPIOC_BASE_ADDRESS + 0x18))
+/* Pointer of struct that points to base address of each port */
+#define MGPIOA ((volatile GPIO*) 0x40010800)
+#define MGPIOB ((volatile GPIO*) 0x40010C00)
+#define MGPIOC ((volatile GPIO*) 0x40011000)
 
 
 
