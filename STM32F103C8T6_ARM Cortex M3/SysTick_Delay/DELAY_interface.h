2c5da29964e43d58b1c9b06e9cf0b3b66f480ddc
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-16 09:21:29 +0200

    Create DELAY_interface.h

---
+++ STM32F103C8T6_ARM Cortex M3/SysTick_Delay/DELAY_interface.h
@@ -0,0 +1,20 @@
+/*****************************************************/
+/* Author      : AbdElrahman I.Zaki                  */
+/* Version     : v02                                 */
+/* date        : 14/8/2020                           */
+/* Description : Delay funtion function prototype	  */
+/*****************************************************/
+
+
+
+#ifndef _DELAY_INTERFACE_H_
+#define _DELAY_INTERFACE_H_
+
+/* Function that set a delay in milli Seconds */
+void _delay_mS(u32 Copy_u32Time);
+
+/* Function that set a delay in micro Seconds */
+void _delay_uS(u32 Copy_u32Time);
+
+
+#endif 
