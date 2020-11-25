e1e5f9cac08ea4c3f3f4d7428e71e9529ae31e55
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-25 12:02:05 +0200

    Create USART_private.h

---
+++ STM32F103C8T6_ARM Cortex M3/USART/USART_private.h
@@ -0,0 +1,39 @@
+/****************************************************************************************/
+/* Author : AbdElrahman I.Zaki                                                          */
+/* Date   : 19 September 2020                                                           */
+/* Version: V02                                                                         */
+/* Desc.  : STM32F103C8T6 ARM Cortex M3 USART Communication Protocol private            */
+/*              functions prototype and registers definition                            */
+/****************************************************************************************/
+
+
+#ifndef USART_PRIVATE_H
+#define USART_PRIVATE_H
+
+
+typedef struct 
+{
+    volatile u32 USART_SR;      /* Status Register */
+    volatile u32 USART_DR;      /* Data Register */
+    volatile u32 USART_BRR;     /* Baud Rate Register */
+    volatile u32 USART_CR1;     /* Control Register 1 */
+    volatile u32 USART_CR2;     /* Control Register 2 */
+    volatile u32 USART_CR3;     /* Control Register 3 */
+    volatile u32 USART_GRPR;    /* Guard Time and Prescaler Register */
+}USART_Type;
+
+
+#define USART1_Ptr  ((volatile USART_Type*) 0x40013800)
+
+#define USART2_Ptr  ((volatile USART_Type*) 0x40004400)
+
+#define USART3_Ptr  ((volatile USART_Type*) 0x40004800)
+
+#define USART4_Ptr  ((volatile USART_Type*) 0x40004C00)
+
+#define USART5_Ptr  ((volatile USART_Type*) 0x40005000)
+
+
+
+
+#endif
