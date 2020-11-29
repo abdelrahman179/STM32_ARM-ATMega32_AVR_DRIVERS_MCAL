ac5788ace28ef8799d72fbc4efd8016b1acd112c
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-11-29 08:21:00 +0200

    Update USART_config.h

--- STM32F103C8T6_ARM Cortex M3/USART/USART_config.h
+++ STM32F103C8T6_ARM Cortex M3/USART/USART_config.h
@@ -8,36 +8,12 @@
 #ifndef USART_CONFIG_H
 #define USART_CONFIG_H
 
-
-/* System Clock */
-#define F_CPU                    8000000UL
-
 /* USART1_Configuration */
+
+/*  USART_1 Setting
+     Options : - USART1_ENABLE
+               - USART1_DISABLE  */
 #define USART1_STATUS            USART1_ENABLE
-
-/*  Baud Rate Configuration
-     Options : - 4800
-               - 9600 
-               - 115200  */
-#define USART1_BAUD_RATE         (u32)9600
-
-/* Word Length 
-    Options : - _8BIT_WORD_LENGTH
-              - _9BIT_WORD_LENGTH  */
-#define USART1_WORD_LENGTH       _8BIT_WORD_LENGTH
-
-/* Parity Check Bit 
-    Options: - PARITY_DISABLE
-             - EVEN_PARITY
-             - ODD_PARITY     */
-#define USART1_PARITY            PARITY_DISABLE
-
-/* USART1 Interrupt Configuration 
-    Options : - INT_DISABLE
-              - TXE_INT_ENABLE
-              - TCE_INT_ENABLE
-              - RXNE_INT_ENABLE    */
-#define USART1_INTERRUPT         INT_DISABLE
 
 /* Fire an interrupt on Transmission 
     Options : - TRANSMITTER_DISABLE 
@@ -48,6 +24,38 @@
     Options : - RECEIVER_DISABLE 
               - RECEIVER_ENABLE         */
 #define USART1_RECEIVER          RECEIVER_ENABLE
+
+/*  Baud Rate Configuration
+     Options : - BAUD_9600 
+               - BAUD_115200  */
+#define USART1_BAUD_RATE         BAUD_115200
+
+/* Word Length 
+    Options : - _8BIT_WORD_LENGTH
+              - _9BIT_WORD_LENGTH  */
+#define USART1_WORD_LENGTH       _8BIT_WORD_LENGTH
+
+/* Parity Check Bit Setting
+    Options: - PARITY_DISABLE
+             - PARITY_ENABLE  */
+#define USART1_PARITY        PARITY_DISABLE
+
+/* Parity Check Bit Configuration
+    Options: - USART1_PARITY_EVEN
+             - USART1_PARITY_ODD  */
+#define USART1_PARITY_TYPE       USART1_PARITY_EVEN      
+
+/* Parity Error Check flag Interrupt
+    Options: - USART_PE_DISABLED
+             - USART_PE_ENABLED  */
+#define PARITY_ERROR_INTERRUPT     USART_PE_DISABLED
+
+/* USART1 Interrupt Configuration 
+    Options : - INT_DISABLE
+              - TXE_INT_ENABLE
+              - TCE_INT_ENABLE
+              - RXNE_INT_ENABLE    */
+#define USART1_INTERRUPT         INT_DISABLE
 
 /* Stop Bit Configurations 
     Options : - ONE_STOP_BIT
