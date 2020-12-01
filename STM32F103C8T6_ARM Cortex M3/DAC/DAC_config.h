b341b85a61acf3ea91f824efc56812c00b467c37
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-12-01 18:09:38 +0200

    Create DAC_config.h

---
+++ STM32F103C8T6_ARM Cortex M3/DAC/DAC_config.h
@@ -0,0 +1,19 @@
+/****************************************************************************/
+/* Author : AbdElrahman I.Zaki                                              */
+/* Date   : 7 September 2020                                                */
+/* Version: V02                                                             */
+/* Desc.  : STM32F103C8T6 ARM Cortex M3 DAC User Manual                     */
+/****************************************************************************/
+
+
+#ifndef DAC_CONFIG_H
+#define DAC_CONFIG_H
+
+#define DAC_PORT    GPIOA
+
+#define DAC_PORT_LH     LOW
+
+
+
+
+#endif
