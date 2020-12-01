6a4d73e5a99b6034f5d18e4bc1fe7c871a4ef8a2
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-12-01 18:09:24 +0200

    Create DAC_interface.h

---
+++ STM32F103C8T6_ARM Cortex M3/DAC/DAC_interface.h
@@ -0,0 +1,17 @@
+/****************************************************************************/
+/* Author : AbdElrahman I.Zaki                                              */
+/* Date   : 7 September 2020                                                */
+/* Version: V02                                                             */
+/* Desc.  : STM32F103C8T6 ARM Cortex M3 DAC Pulic Functions Prototypes      */
+/****************************************************************************/
+
+#ifndef DAC_INTERFACE_H
+#define DAC_INTERFACE_H
+
+void MDAC_voidInit(void);
+
+void MDAC_voidPlay(void);
+
+
+
+#endif
